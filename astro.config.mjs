350be05d6715634f837f236719597c2ebf1910a5
Jakub Gortych <jakub.gortych@huuugegames.com>
2025-05-27 14:07:59 +0200

    Add deplyment action fixes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,10 @@ export default defineConfig({
     },
     ssr: {
       noExternal: ["msw"],
+      external: ["react", "react-dom"],
+    },
+    define: {
+      global: "globalThis",
     },
   },
   adapter: cloudflare({
